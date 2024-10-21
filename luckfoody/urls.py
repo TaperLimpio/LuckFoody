24737a4c23a77f6dd348bd309a10a6b67a18de36
TaperLimpio <167125666+TaperLimpio@users.noreply.github.com>
2024-10-21 20:37:22 -0300

    se arreglo cambios ocacionados por git

--- luckfoody/urls.py
+++ luckfoody/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from PaginaWeb.views import portalpago,ingresarplatillo,verplatillo,pagofracaso,pagoexitoso,ingresarcatalogo
+from PaginaWeb.views import portalpago,ingresarplatillo,carritocompra,verplatillo,pagofracaso,pagoexitoso,ingresarcatalogo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('portal_pago/',portalpago),
     path('ingresar_platillo/',ingresarplatillo),
+    path('carrito_de_compra/',carritocompra),
     path('pago fracaso/',pagofracaso),
     path('pago exitoso/',pagoexitoso),
     path('ingresar catalogo/',ingresarcatalogo),
